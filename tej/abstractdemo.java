6229ec1fc3ecc9c0059fcb620b4c5fff4a6ddda8
Vaibhav17441744 <vaibhavsyadav1101@gmail.com>
2024-10-13 18:29:50 +0530

    1st update\

---
+++ tej/abstractdemo.java
@@ -0,0 +1,37 @@
+
+public class abstractdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         
+	}
+
+}
+
+
+abstract class Electronics{
+	int rollno;
+	abstract void area() ;
+	abstract void on();
+	public void m1() {
+		
+		
+	}
+}
+class Cb extends Electronics{
+
+	@Override
+	void area() {
+		
+		
+	}
+
+	@Override
+	void on() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
